cf54ef82386d926292fcb3dfb2c5c6cff15d25ab
drowsy <away28618@gmail.com>
2024-06-26 00:06:02 +0530

    fixed secret leak

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs"
 
 cloudinary.config({ 
-    cloud_name: 'dwflmkssm', 
-    api_key: '147193313958721', 
-    api_secret: 'VxnQJU-Z_9iYq6F1IdpwOcxjpx0'
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 
