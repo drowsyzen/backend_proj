7fdbf01d9af0e8fb03e4325026de51b28517a7f5
drowsy <away28618@gmail.com>
2024-06-26 21:39:31 +0530

    added login Api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,12 +4,31 @@ import { User } from "../models/users.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import ApiResponse from "../utils/apiResponse.js"
 
+
 // const registerUser = asyncHandler( async (req,res) => {
 //     console.log('IN the get req')
 //     res.status(200).json({
 //         message:"ok"
 //     })
 // })
+
+const generateAccessAndRefreshToken = async (userid) => {
+    try{
+
+        const usrobej = await User.findById({_id:userid})
+        const accessToken = await usrobej.generateAccessToken()
+        const refreshToken = await usrobej.generateRefreshToken()
+
+        usrobej.refreshToken = refreshToken
+        await usrobej.save({validateBeforeSave:false}) 
+
+        return { accessToken, refreshToken }
+
+    }
+    catch(error){
+        throw new Apierror(502,"something went wrong while generating the tokens.")
+    }
+}
 
 const registerUser = asyncHandler( async (req,res) => {
     console.log('IN the get req')
@@ -75,15 +94,69 @@ const registerUser = asyncHandler( async (req,res) => {
 })
 
 const getUser = asyncHandler( async (req,res) => {
-    console.log('IN the get req')
-    const users = User.findOne()
-    console.log('IN the get req',users)
+    // console.log('IN the get req')
+    // const users = User.find()
+    const uquery = User.findOne({'username':'qwerty1'}).select('-password').exec().then(
+        console.log()
+    )
+
+    console.log('IN -->',uquery)
+    // console.log('IN the get req',users.fullName)
     
-    return res.status(201).json(
-        new ApiResponse(201,users,"User added successfully."))
+    // return res.status(201).json(
+    //     new ApiResponse(201,users,"User added successfully."))
     
+    return res.status(202).json(
+        new ApiResponse(201,"users","User added successfully."))
 })
 
+
+
+const login = asyncHandler( async (req,res) => {
+    // console.log('IN the get req')
+    // const users = User.find()
+    const {username ,email, password} = req.body
+
+    if (!username || !email ){
+        throw new Apierror(406,"username or email is required")
+    }
+
+    const usrobj = User.findOne({
+        $or:[ {username}, {email}]
+    })
+
+    if (!usrobj) {
+        throw new Apierror(407,"User does not exists")
+    }
+
+    const passwordcheck = await usrobj.isPasswordCorrect(password)
+
+    if (!passwordcheck){
+
+        throw new Apierror(408,'Password is not correct')
+    }
+
+    const { acsToken,refshToken } = await generateAccessAndRefreshToken(usrobj._id)
+
+    const loggedinUser = User.findById(usrobj._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+            .status(200)
+            .cookie("accessToken",acsToken,options)
+            .cookie("refreshToken",refshToken,options)
+            .json(
+                new ApiResponse(200,{
+                    user:loggedinUser,acsToken,refshToken
+                })
+            )
+
+
+})
 
 
 // export default registerUser, getUser;
